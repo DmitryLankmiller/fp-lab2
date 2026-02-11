06d8ef1287d7d03ca916cff296f657704b5d71dd
Dmitriy Astrov <dskurochka@gmail.com>
2026-02-11 18:58:57 +0300

    Simplified pbt

--- test/trie_pbt_test.clj
+++ test/trie_pbt_test.clj
@@ -47,6 +47,9 @@
 (def gen-trie
   (gen/fmap build-trie gen-entries-map))
 
+(def gen-not-empty-trie
+  (gen/fmap build-trie (gen/such-that (comp not empty?) gen-entries-map)))
+
 (def gen-int-entries-map
   (gen/map gen-key gen/small-integer {:min-elements 0 :max-elements 60}))
 
@@ -67,14 +70,12 @@
                 (is (true? (t/tequals? (t/empty-trie) (t/empty-trie))))))
 
 (defspec pbt-not-equals-empty-for-nonempty iteration-num
-  (prop/for-all [m (gen/such-that (comp not empty?) gen-entries-map 50)]
-                (let [tr (build-trie m)]
-                  (is (false? (t/tequals? tr (t/empty-trie)))))))
+  (prop/for-all [tr gen-not-empty-trie]
+                (is (false? (t/tequals? tr (t/empty-trie))))))
 
 (defspec pbt-get-entries-two-way iteration-num
-  (prop/for-all [m gen-entries-map]
-                (let [tr  (build-trie m)
-                      m2  (trie->map tr)
+  (prop/for-all [tr gen-trie]
+                (let [m  (trie->map tr)
                       tr2 (build-trie m2)]
                   (is (t/tequals? tr tr2)))))
 
@@ -89,81 +90,37 @@
                   (is (= v (t/tget tr k))))))
 
 (defspec pbt-insert-then-get iteration-num
-  (prop/for-all [m gen-entries-map
+  (prop/for-all [tr gen-trie
                  k gen-key
                  v gen-value]
-                (let [tr (build-trie m)
-                      tr2 (t/insert tr k v)]
+                (let [tr2 (t/insert tr k v)]
                   (is (= v (t/tget tr2 k))))))
 
 (defspec pbt-delete-from-empty-is-empty iteration-num
   (prop/for-all [k gen-key]
                 (is (t/tequals? (t/delete (t/empty-trie) k) (t/empty-trie)))))
 
-(defspec pbt-delete-existing-removes-key iteration-num
-  (prop/for-all [m (gen/such-that (comp not empty?) gen-entries-map 50)]
-                (let [k   (first (keys m))
-                      tr  (build-trie m)
-                      tr2 (t/delete tr k)]
-                  (is (not (nil? (t/tget tr k))))
-                  (is (nil? (t/tget tr2 k))))))
-
 (defspec pbt-filter-empty-is-empty iteration-num
   (prop/for-all [_ gen/boolean]
                 (is (t/tequals? (t/tfilter (t/empty-trie) (fn [_ _] true))
                                 (t/empty-trie)))))
-
-(defspec pbt-filter-correctness iteration-num
-  (prop/for-all [m gen-entries-map
-                 pred-kind (gen/elements [:all :none :short-key :number :bool :kw :str])]
-                (let [pred (case pred-kind
-                             :all       (fn [_ _] true)
-                             :none      (fn [_ _] false)
-                             :short-key (fn [k _] (< (count k) 4))
-                             :number    (fn [_ v] (number? v))
-                             :bool      (fn [_ v] (boolean? v))
-                             :kw        (fn [_ v] (keyword? v))
-                             :str       (fn [_ v] (string? v)))
-                      tr (build-trie m)
-                      tr2 (t/tfilter tr pred)
-                      m2 (trie->map tr2)
-                      expected (into {} (filter (fn [[ks v]]
-                                                  (pred (vec (seq ks)) v))) m)]
-                  (is (= expected m2)))))
 
 (defspec pbt-map-empty-is-empty iteration-num
   (prop/for-all [_ gen/boolean]
                 (is (t/tequals? (t/tmap (t/empty-trie) (fn [_ _] [(vec (seq "x")) 1]))
                                 (t/empty-trie)))))
 
-(defspec pbt-map-correctness iteration-num
-  (prop/for-all [m gen-entries-map
-                 kind (gen/elements [:id :inc-val :injective-key])]
-                (let [f (case kind
-                          :id (fn [k v] [k v])
-                          :inc-val (fn [k v] [k (if (number? v) (inc v) v)])
-                          :injective-key (fn [k v] [(conj k \x) v]))
-                      tr (build-trie m)
-                      tr2 (t/tmap tr f)
-                      m2 (trie->map tr2)
-                      expected (case kind
-                                 :id m
-                                 :inc-val (into {} (map (fn [[ks v]] [ks (if (number? v) (inc v) v)])) m)
-                                 :injective-key (into {} (map (fn [[ks v]] [(str ks "x") v])) m))]
-                  (is (= expected m2)))))
-
 (defspec pbt-reducel-empty-returns-acc iteration-num
   (prop/for-all [acc gen/small-integer]
                 (is (= acc (t/reducel (t/empty-trie) (fn [a _ _] (inc a)) acc)))))
 
 (defspec pbt-reduces-sum-and-left-equals-right iteration-num
-  (prop/for-all [m gen-int-entries-map
+  (prop/for-all [tr gen-int-trie
                  acc gen/small-integer]
-                (let [tr (build-trie m)
-                      f  (fn [a _ v] (+ a v))
-                      expected (+ acc (reduce + 0 (vals m)))]
-                  (is (= expected (t/reducel tr f acc)))
-                  (is (= expected (t/reducer tr f acc))))))
+                (let [f  (fn [a _ v] (+ a v))
+                      res-left (t/reducel tr f acc)
+                      res-right (t/reducer tr f acc)]
+                  (is (= res-left res-right)))))
 
 ;; Monads
 
